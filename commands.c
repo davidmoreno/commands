6582b82840cc0a831f5e33f931ead783915907c3
David Moreno <dmoreno@coralbits.com>
2014-12-23 20:58:43 +0100

    Added a small note on where to obtain the original commands.c.

--- commands.c
+++ commands.c
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * Originally distributed at https://github.com/davidmoreno/commands . 
+ * Change at free will.
  */
 
 #define _GNU_SOURCE
